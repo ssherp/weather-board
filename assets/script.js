fd4b8cec8ec07f65be15b40ae8aa0675e9705998
ssherp <ssherpa89@gmail.com>
2023-07-10 21:31:29 -0700

    weather api data

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 $("#search-btn").on("click",function(){
     
 var city = $(this).siblings(".form-control").val();
@@ -21,11 +27,13 @@ localStorage.setItem(key,JSON.stringify(cityArr));
 
 function getCity(city){
 
-var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=7c352849c8e0a97299331906dbac363a`
+// var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=7c352849c8e0a97299331906dbac363a`
+var requestUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7c352849c8e0a97299331906dbac363a&units=metric`
 
 fetch (requestUrl)
 
 .then(function(response){
 
 console.log(response.json())
+
 })}
